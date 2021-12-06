6245f2a3b73d6c19034d8c7688d7f60b7331dcb9
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2021-12-06 18:04:53 -0300

    90.6% test coverage. testing generateEditingControllerMapping function

--- lib/controller/flutter_json_schema_form_controller.dart
+++ lib/controller/flutter_json_schema_form_controller.dart
@@ -3,6 +3,7 @@ import 'package:json_schema_document/json_schema_document.dart';
 
 class FlutterJsonSchemaFormController {
   Map<String, dynamic> data = {};
+  Map<String, dynamic> controllerMapping = {};
 
   /// Create or update a value of the key on the specified path.
   void updateValue(List<String> path, String value) {
@@ -27,19 +28,53 @@ class FlutterJsonSchemaFormController {
         if (i == 0) {
           // if this case is the last case, then we need to update the value
           if (updaterIndex == 0) {
+            // update the value on the data field
             data[p] = value;
+            // update or create the controller
+            if (controllerMapping[p] is TextEditingController) {
+              (controllerMapping[p] as TextEditingController).text = value;
+            } else if (controllerMapping[p] == null) {
+              controllerMapping[p] = TextEditingController(text: value);
+            }
+
             // if this case isn't the last case, then we need to create de object
           } else {
             if (data[p] == null) {
+              // create the object
               data[p] = {};
             }
             last = p;
           }
           // case 1: the second element only can be a value in this especific situation
         } else {
-          data[last][p] = value;
+          data[last]?[p] = value;
+          // update or create the controller
+          if (controllerMapping[last]?[p] is TextEditingController) {
+            (controllerMapping[last]?[p] as TextEditingController).text = value;
+          } else if (controllerMapping[last]?[p] == null) {
+            controllerMapping[last]?[p] = TextEditingController(text: value);
+          }
         }
       }
     }
   }
+}
+
+/// Takes as an argument a [JsonSchema] and return a Map<String, dynamic> where
+/// the keys are the names of the properties and the values are instances of
+/// [TextEditingController] for the corresponding properties on the
+/// FlutterJSONSchemaForm or similar maps on recursive fashion
+Map<String, dynamic> generateEditingControllerMapping(JsonSchema jsonSchema) {
+  // for each property on the JSON Schema
+  return jsonSchema.properties.map((key, value) {
+    // if the property is an object
+    if (value.type == JsonSchemaType.object) {
+      // if the property is an object, then we need to create a map with the
+      // same name and the same type of the object
+      return MapEntry(key, generateEditingControllerMapping(value));
+    } else {
+      // if the property is a value, then we need to create a controller
+      return MapEntry(key, TextEditingController());
+    }
+  });
 }
--- test/flutter_json_schema_form_controller_test.dart
+++ test/flutter_json_schema_form_controller_test.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_json_schema_form/controller/flutter_json_schema_form_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:json_schema_document/json_schema_document.dart';
 
 void main() {
   group('updateValue method tests', () {
@@ -14,6 +16,11 @@ void main() {
       expect(controller.data['test'], 'testValue2');
     });
 
+    // Should create or update value of the key on the specified path on the corresponding controller - nesting of level 1
+    test(
+        'Should create or update value of the key on the specified path on the corresponding controller - nesting of level 1',
+        () {});
+
     // Should create or update value of the key on the specified path - nesting of level 2
     test(
         'Should create or update value of the key on the specified path - nesting of 2 levels',
@@ -23,6 +30,11 @@ void main() {
       expect(controller.data['coordinates']?['latitude'], '-23.5');
     });
 
+    // Should create or update value of the key on the specified path on the corresponding controller - nesting of level 2
+    test(
+        'Should create or update value of the key on the specified path on the corresponding controller - nesting of level 1',
+        () {});
+
     // Should create or update value of the key on the specified path - nesting of level 3
     // test(
     //     'Should create or update value of the key on the specified path - nesting of 3 levels',
@@ -31,5 +43,62 @@ void main() {
     //   controller.updateValue(['coordinates', 'latitude', 'decimal'], '-23.5');
     //   expect(controller.data['coordinates']?['latitude']?['decimal'], '-23.5');
     // });
+  });
+
+  // generateEditingControllerMapping tests
+  group('generateEditingControllerMapping tests', () {
+    // Should generate surface level mapping on non-nested jsonSchema
+    test('Should generate surface level mapping on non-nested jsonSchema', () {
+      final controllerMapping =
+          generateEditingControllerMapping(JsonSchema.fromMap({
+        "type": "object",
+        "title": "Login",
+        "properties": {
+          "username": {
+            "type": "string",
+            "title": "Username",
+          },
+          "password": {
+            "type": "string",
+            "title": "Password",
+          },
+        },
+      }));
+      expect(controllerMapping['username'] is TextEditingController, true);
+    });
+
+    // Should generate corresponding map on nested jsonSchema
+    test('Should generate corresponding map on nested jsonSchema', () {
+      final controllerMapping =
+          generateEditingControllerMapping(JsonSchema.fromMap({
+        "type": "object",
+        "title": "Login",
+        "properties": {
+          "coordinates": {
+            "type": "object",
+            "title": "Coordinates",
+            "properties": {
+              "latitude": {
+                "type": "number",
+                "title": "Latitude",
+              },
+              "longitude": {
+                "type": "number",
+                "title": "Longitude",
+              },
+            },
+          },
+        },
+      }));
+      expect(controllerMapping['coordinates'] is Map, true);
+      expect(
+          controllerMapping['coordinates']?['latitude']
+              is TextEditingController,
+          true);
+      expect(
+          controllerMapping['coordinates']?['longitude']
+              is TextEditingController,
+          true);
+    });
   });
 }
