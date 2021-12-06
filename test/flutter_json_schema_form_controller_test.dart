2f437ffb5ee7072aa1fbb5d5953593f6f87749fb
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2021-12-06 17:23:19 -0300

    88.9% test coverage. updateValue nesting 0 and nesting 1 tested

--- test/flutter_json_schema_form_controller_test.dart
+++ test/flutter_json_schema_form_controller_test.dart
@@ -1,5 +1,35 @@
+import 'package:flutter_json_schema_form/controller/flutter_json_schema_form_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('', () {});
+  group('updateValue method tests', () {
+    // Should create or update value of the key on the specified path - nesting of level 1
+    test(
+        'Should create or update value of the key on the specified path - nesting of level 1',
+        () {
+      final controller = FlutterJsonSchemaFormController();
+      controller.updateValue(['test'], 'testValue');
+      expect(controller.data['test'], 'testValue');
+      controller.updateValue(['test'], 'testValue2');
+      expect(controller.data['test'], 'testValue2');
+    });
+
+    // Should create or update value of the key on the specified path - nesting of level 2
+    test(
+        'Should create or update value of the key on the specified path - nesting of 2 levels',
+        () {
+      final controller = FlutterJsonSchemaFormController();
+      controller.updateValue(['coordinates', 'latitude'], '-23.5');
+      expect(controller.data['coordinates']?['latitude'], '-23.5');
+    });
+
+    // Should create or update value of the key on the specified path - nesting of level 3
+    // test(
+    //     'Should create or update value of the key on the specified path - nesting of 3 levels',
+    //     () {
+    //   final controller = FlutterJsonSchemaFormController();
+    //   controller.updateValue(['coordinates', 'latitude', 'decimal'], '-23.5');
+    //   expect(controller.data['coordinates']?['latitude']?['decimal'], '-23.5');
+    // });
+  });
 }
