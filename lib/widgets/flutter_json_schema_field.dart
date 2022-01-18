efe64294f327ab7739765f29bda841f91121936c
Gabrimarin <marinho.ifce@gmail.com>
2022-01-18 12:39:23 -0300

    Starting to add the possibility to remove a file

--- lib/widgets/flutter_json_schema_field.dart
+++ lib/widgets/flutter_json_schema_field.dart
@@ -35,6 +35,41 @@ class FlutterJsonSchemaFormField extends StatelessWidget {
 
   bool get enabled => !readOnly;
 
+  Future<void> onUpload() async {
+    // print(controller.data['svgProp']);
+    final svgProp = controller.data['svgProp'];
+    if (svgProp != null) {
+      final newData = controller.data;
+      newData['svgProp'] = null;
+      controller.setData(newData);
+      return;
+    }
+    // print(controller.data[pa])
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      final file = result.files.first;
+      final extension = file.extension;
+      if (extension == "svg") {
+        controller.addFile(file);
+        // const type = "image/svg+xml";
+        // final data = base64.encode(file.bytes ?? []);
+        // final fileName = file.name;
+        // final size = file.size;
+        // final lastModified =
+        //     DateTime.now().toUtc().toIso8601String();
+        // controller.data['svgProp'] = {
+        //   'name': fileName,
+        //   'size': size,
+        //   'type': type,
+        //   'lastModified': lastModified,
+        //   'data': data,
+        // };
+      }
+    } else {
+      // User canceled the picker
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     switch (jsonSchema.type) {
@@ -72,44 +107,39 @@ class FlutterJsonSchemaFormField extends StatelessWidget {
       case JsonSchemaType.object:
         final svgProps = ['name', 'size', 'type', 'lastModified', 'data'];
         if (listEquals(svgProps, jsonSchema.properties.keys.toList())) {
-          return Row(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                    // controller.data['svgProp']?['name'] ??
-                    'Importar arquivo'), // Currently not working
-              ),
-              IconButton(
-                icon: const Icon(Icons.upload_file),
-                onPressed: () async {
-                  FilePickerResult? result =
-                      await FilePicker.platform.pickFiles();
-                  if (result != null) {
-                    final file = result.files.first;
-                    final extension = file.extension;
-                    if (extension == "svg") {
-                      controller.addFile(file);
-                      // const type = "image/svg+xml";
-                      // final data = base64.encode(file.bytes ?? []);
-                      // final fileName = file.name;
-                      // final size = file.size;
-                      // final lastModified =
-                      //     DateTime.now().toUtc().toIso8601String();
-                      // controller.data['svgProp'] = {
-                      //   'name': fileName,
-                      //   'size': size,
-                      //   'type': type,
-                      //   'lastModified': lastModified,
-                      //   'data': data,
-                      // };
-                    }
-                  } else {
-                    // User canceled the picker
-                  }
-                },
-              )
-            ],
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    title ?? 'File',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                        onPressed: forceDisabled ? null : onUpload,
+                        child: Row(children: [
+                          controller.data['svgProp'] == null
+                              ? Icon(Icons.file_upload)
+                              : Icon(Icons.delete),
+                          controller.data['svgProp'] == null
+                              ? Text('Importar')
+                              : Text('Remover'),
+                        ])),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(controller.data['svgProp']?['name'] ??
+                          'Nenhum arquivo'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         } else {
           return Column(
